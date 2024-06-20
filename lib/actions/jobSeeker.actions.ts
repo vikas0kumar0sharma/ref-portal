79e6d215066214928dbaa973f848c0ef1124423d
vikas0kumar0sharma <vsharma@betatestsolutions.com>
2024-06-20 23:06:21 +0530

    type issue

--- lib/actions/jobSeeker.actions.ts
+++ lib/actions/jobSeeker.actions.ts
@@ -1,43 +1,38 @@
-"use server"
+"use server";
 
-import JobSeeker from "../models/jobSeeker.model"
-import {connectToDB} from "../mongoose"
+import JobSeeker from "../models/jobSeeker.model";
+import { connectToDB } from "../mongoose";
 
-export async function fetchJobSeeker(jobSeekerId:string){
-    try {
-       
-        connectToDB();
+export async function fetchJobSeeker(jobSeekerId: string) {
+  try {
+    connectToDB();
 
-        const jobSeeker=await JobSeeker.findOne({id:jobSeekerId})
+    const jobSeeker = await JobSeeker.findOne({ id: jobSeekerId });
 
-        return jobSeeker
-        
-    } catch (error:any) {
-        throw new Error(`Failed to fetch user:${error.message}`)
-    }
+    return jobSeeker;
+  } catch (error: any) {
+    throw new Error(`Failed to fetch user:${error.message}`);
+  }
 }
 
-export async function addJobSeeker(data) {
-    try {
-        
-        connectToDB()
+export async function addJobSeeker(data: any) {
+  try {
+    connectToDB();
 
-        await JobSeeker.create({
-            id:data.id,
-            email:data.email,
-            FirstName:data.FirstName,
-            LastName:data.LastName,
-            CollegeName:data.CollegeName,
-            CGPA:data.CGPA,
-            YOG:data.YOG,
-            PhoneNo:data.PhoneNo,
-            YOE:data.YOE,
-            ResumeLink:data.ResumeLink,
-            LinkedInProf:data.LinkedInProf
-        })
-        
-    } catch (error:any) {
-        throw new Error(`Failed to create user:${error.message}`)
-    }
-
+    await JobSeeker.create({
+      id: data.id,
+      email: data.email,
+      FirstName: data.FirstName,
+      LastName: data.LastName,
+      CollegeName: data.CollegeName,
+      CGPA: data.CGPA,
+      YOG: data.YOG,
+      PhoneNo: data.PhoneNo,
+      YOE: data.YOE,
+      ResumeLink: data.ResumeLink,
+      LinkedInProf: data.LinkedInProf,
+    });
+  } catch (error: any) {
+    throw new Error(`Failed to create user:${error.message}`);
+  }
 }
