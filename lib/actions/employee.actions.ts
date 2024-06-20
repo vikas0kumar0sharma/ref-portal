cf4708ec83c818ea0324fe1a89c858da12470fdd
vikas0kumar0sharma <vsharma@betatestsolutions.com>
2024-06-20 23:02:35 +0530

    type issue.

--- lib/actions/employee.actions.ts
+++ lib/actions/employee.actions.ts
@@ -1,41 +1,35 @@
-"use server"
+"use server";
 
-import Employee from "../models/employee.model"
-import {connectToDB} from "../mongoose"
+import Employee from "../models/employee.model";
+import { connectToDB } from "../mongoose";
 
-export async function fetchEmployee(employeeId:string){
-    try {
-       
-        connectToDB();
+export async function fetchEmployee(employeeId: string) {
+  try {
+    connectToDB();
 
-        const employee=await Employee.findOne({id:employeeId})
+    const employee = await Employee.findOne({ id: employeeId });
 
-        return employee
-        
-    } catch (error:any) {
-        throw new Error(`Failed to fetch user:${error.message}`)
-    }
+    return employee;
+  } catch (error: any) {
+    throw new Error(`Failed to fetch user:${error.message}`);
+  }
 }
 
-export async function addEmployee(data) {
-    
-    try {
-        
-        connectToDB()
+export async function addEmployee(data: any) {
+  try {
+    connectToDB();
 
-        await Employee.create({
-            id:data.id,
-            email:data.email,
-            FirstName:data.FirstName,
-            LastName:data.LastName,
-            CompanyName:data.CompanyName.toUpperCase(),
-            JobTitle:data.JobTitle,
-            YOE:data.YOE,
-            LinkedInProf:data.LinkedInProf
-        })
-
-    } catch (error:any) {
-        throw new Error(`Failed to create user:${error.message}`)
-    }
-
+    await Employee.create({
+      id: data.id,
+      email: data.email,
+      FirstName: data.FirstName,
+      LastName: data.LastName,
+      CompanyName: data.CompanyName.toUpperCase(),
+      JobTitle: data.JobTitle,
+      YOE: data.YOE,
+      LinkedInProf: data.LinkedInProf,
+    });
+  } catch (error: any) {
+    throw new Error(`Failed to create user:${error.message}`);
+  }
 }
